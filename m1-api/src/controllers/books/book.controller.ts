1f27d954a22136ea2d937d60485c2bad11f17651
Romain <romain.pietri@student.junia.com>
2024-11-14 23:18:27 +0100

    Comit vide

--- m1-api/src/controllers/books/book.controller.ts
+++ m1-api/src/controllers/books/book.controller.ts
@@ -10,7 +10,6 @@ type File = Express.Multer.File;
 @Controller('api/books')
 export class BookController {
     constructor(private readonly bookService: BookService) {}
-
     // Route pour obtenir la liste des livres avec option de recherche et de tri
     @Get()
     async getBooks(
